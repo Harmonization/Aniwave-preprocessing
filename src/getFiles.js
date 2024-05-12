3cd9184db14b0a791b9ba1946259a3a803b7cf1f
Max <44732792+Harmonization@users.noreply.github.com>
2024-05-12 18:01:59 +0300

    fix

--- src/getFiles.js
+++ src/getFiles.js
@@ -1,9 +1,16 @@
 
 export const getFiles = async () => {
     // Загрузка длин волн
-    const response_nm = await fetch('./Static/nm.json')
-    const nm = await response_nm.json()
-
+    // const response_nm = await fetch('./Static/nm.json')
+    
+    const response = await fetch('./Static/nm.json', {
+        headers:{
+            accept: 'application/json',
+            'User-agent': 'learning app',
+        }
+    });
+    const nm = await response.json()
+    
     // Загрузка текста описания разделов
     // const response_text = await fetch('./static/text.json')
     // const text = await response_text.json()
